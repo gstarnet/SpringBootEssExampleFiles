92e1571b680e1461379feeb0c9dd8d0d81f210be
Gregory Drogaline <greg.drogaline@gmail.com>
2019-01-26 15:49:49 -0600

    Health endpoint

--- room-web-app/src/main/java/com/frankmoley/boot/landon/roomwebapp/RoomServices.java
+++ room-web-app/src/main/java/com/frankmoley/boot/landon/roomwebapp/RoomServices.java
@@ -1,19 +1,30 @@
 package com.frankmoley.boot.landon.roomwebapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class RoomServices {
+public class RoomServices implements HealthIndicator {
     private RoomRepository roomRepository;
 
     @Autowired
     public RoomServices(RoomRepository roomRepository){
         super();
         this.roomRepository = roomRepository;
+    }
+
+    @Override
+    public Health health() {
+        List<Room> rooms = this.getAllRooms();
+        if(rooms.size()>1){
+            return Health.up().build();
+        }
+        return Health.down().build();
     }
 
     public List<Room> getAllRooms(){
